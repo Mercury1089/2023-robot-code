984b583fc88d268908661d6cbd74a65b97657961
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-01-11 20:52:56 -0500

    Add button bindings

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,66 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+
+  public static class DS_USB {
+    public static final int LEFT_STICK = 0, RIGHT_STICK = 1, GAMEPAD = 2;
+
+    private DS_USB() {
+    }
   }
+
+  public static class GAMEPAD_BUTTONS {
+    public static final int A = 1;
+    public static final int B = 2;
+    public static final int X = 3;
+    public static final int Y = 4;
+    public static final int LB = 5;
+    public static final int RB = 6;
+    public static final int BACK = 7;
+    public static final int START = 8;
+    public static final int L3 = 9;
+    public static final int R3 = 10;
+
+    private GAMEPAD_BUTTONS() {
+    }
+  }
+
+  /**
+  *From 0 in the up direction, and counter clockwise in degrees (right is 90 and upper left is 315).
+  */
+  public static class GAMEPAD_POV {
+      public static final int MIDDLE = -1;
+      public static final int UP = 0;
+      public static final int UP_RIGHT = 45;
+      public static final int RIGHT = 90;
+      public static final int DOWN_RIGHT = 135;
+      public static final int DOWN = 180;
+      public static final int DOWN_LEFT = 225;
+      public static final int LEFT = 270;
+      public static final int UP_LEFT = 315;
+  }
+  public static class JOYSTICK_BUTTONS {
+    public static final int BTN1 = 1;
+    public static final int BTN2 = 2;
+    public static final int BTN3 = 3;
+    public static final int BTN4 = 4;
+    public static final int BTN5 = 5;
+    public static final int BTN6 = 6;
+    public static final int BTN7 = 7;
+    public static final int BTN8 = 8;
+    public static final int BTN9 = 9;
+    public static final int BTN10 = 10;
+    public static final int BTN11 = 11;
+
+    public JOYSTICK_BUTTONS() {
+    }
+  }
+
+  public static class GAMEPAD_AXIS {
+    public static final int leftX = 0, leftY = 1, leftTrigger = 2, rightTrigger = 3, rightX = 4, rightY = 5;
+
+    private GAMEPAD_AXIS() {
+    }
+  }
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,19 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.Constants.DS_USB;
+import frc.robot.Constants.GAMEPAD_AXIS;
+import frc.robot.Constants.GAMEPAD_BUTTONS;
+import frc.robot.Constants.GAMEPAD_POV;
+import frc.robot.Constants.JOYSTICK_BUTTONS;
+import frc.robot.util.PovButton;
+import frc.robot.util.TriggerButton;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,15 +26,25 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private Joystick rightJoystick, leftJoystick, gamepad;
+
+  private JoystickButton left1, left2, left3, left4, left5, left6, left7, left8, left9, left10, left11;
+  private JoystickButton right1, right2, right3, right4, right5, right6, right7, right8, right9, right10, right11;
+  private JoystickButton gamepadA, gamepadB, gamepadX, gamepadY, gamepadRB, gamepadLB, gamepadL3, gamepadBack, 
+  gamepadStart, gamepadLeftStickButton, gamepadRightStickButton;
+  private TriggerButton gamepadLT, gamepadRT;
+  private PovButton gamepadPOVDown, gamepadPOVUpLeft, gamepadPOVUp, gamepadPOVUpRight;
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
+
+    leftJoystick = new Joystick(DS_USB.LEFT_STICK);
+    rightJoystick = new Joystick(DS_USB.RIGHT_STICK);
+    gamepad = new Joystick(DS_USB.GAMEPAD);
+
+
     configureBindings();
   }
 
@@ -43,12 +59,49 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+        left1 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN1);
+        left2 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN2);
+        left3 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN3);
+        left4 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN4);
+        left5 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN5);
+        left6 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN6);
+        left7 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN7);
+        left8 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN8);
+        left9 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN9);
+        left10 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN10);
+        left11 = new JoystickButton(leftJoystick, JOYSTICK_BUTTONS.BTN11);
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+        right1 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN1);
+        right2 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN2);
+        right3 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN3);
+        right4 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN4);
+        right5 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN5);
+        right6 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN6);
+        right7 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN7);
+        right8 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN8);
+        right9 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN9);
+        right10 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN10);
+        right11 = new JoystickButton(rightJoystick, JOYSTICK_BUTTONS.BTN11);
+
+        gamepadA = new JoystickButton(gamepad, GAMEPAD_BUTTONS.A);
+        gamepadB = new JoystickButton(gamepad, GAMEPAD_BUTTONS.B);
+        gamepadX = new JoystickButton(gamepad, GAMEPAD_BUTTONS.X);
+        gamepadY = new JoystickButton(gamepad, GAMEPAD_BUTTONS.Y);
+        gamepadRB = new JoystickButton(gamepad, GAMEPAD_BUTTONS.RB);
+        gamepadLB = new JoystickButton(gamepad, GAMEPAD_BUTTONS.LB);
+        gamepadBack = new JoystickButton(gamepad, GAMEPAD_BUTTONS.BACK);
+        gamepadStart = new JoystickButton(gamepad, GAMEPAD_BUTTONS.START);
+        gamepadL3 = new JoystickButton(gamepad, GAMEPAD_BUTTONS.L3);
+        gamepadLeftStickButton = new JoystickButton(gamepad, GAMEPAD_BUTTONS.L3);
+        gamepadRightStickButton = new JoystickButton(gamepad, GAMEPAD_BUTTONS.R3);
+        gamepadLT = new TriggerButton(gamepad, GAMEPAD_AXIS.leftTrigger);
+        gamepadRT = new TriggerButton(gamepad, GAMEPAD_AXIS.rightTrigger);
+        
+
+        gamepadPOVDown = new PovButton(gamepad, GAMEPAD_POV.DOWN);
+        gamepadPOVUpLeft = new PovButton(gamepad, GAMEPAD_POV.UP_LEFT);
+        gamepadPOVUp = new PovButton(gamepad, GAMEPAD_POV.UP);
+        gamepadPOVUpRight = new PovButton(gamepad, GAMEPAD_POV.UP_RIGHT);
   }
 
   /**
@@ -58,6 +111,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
