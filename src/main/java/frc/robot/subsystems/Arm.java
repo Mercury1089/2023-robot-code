f2dc51d82b08736e3eff9815a1b1fad3025bef42
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2023-01-24 16:14:08 -0500

    remove unused import (git test)

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.CAN;
 import frc.robot.util.MercMath;
 
 
