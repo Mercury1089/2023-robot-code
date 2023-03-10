b08f202b93a4470f9bb1909b15f7cdc73c2db76c
Owen Chin <97705664+owen-chin@users.noreply.github.com>
2023-03-10 16:59:06 -0500

    TalonSRX -> FX

--- src/main/java/frc/robot/subsystems/arm/Wrist.java
+++ src/main/java/frc/robot/subsystems/arm/Wrist.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
 import com.ctre.phoenix.motorcontrol.SensorTerm;
 import com.ctre.phoenix.motorcontrol.StatusFrame;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 import com.ctre.phoenix.sensors.PigeonIMU_StatusFrame;
@@ -50,11 +51,11 @@ public class Wrist extends SubsystemBase {
   // Need to find Gear ratio 30:1
   public final double GEAR_RATIO = 1;
 
-  private TalonSRX wrist;
+  private TalonFX wrist;
   private PigeonIMU pigeon;
 
   public Wrist() {
-    wrist = new TalonSRX(CAN.WRIST_TALON);
+    wrist = new TalonFX(CAN.WRIST_TALON);
     // Configure Gyro
     pigeon = new PigeonIMU(CAN.ARM_GYRO);
     pigeon.configFactoryDefault();
@@ -62,13 +63,8 @@ public class Wrist extends SubsystemBase {
 
     wrist.configFactoryDefault();
 
-    wrist.setSensorPhase(true);
+    wrist.setSensorPhase(false);
     wrist.setInverted(false);
-
-    //wrist.configForwardSoftLimitThreshold(MercMath.degreesToEncoderTicks(WRIST_UPPER_LIMIT)*GEAR_RATIO, Constants.CTRE_TIMEOUT);
-    //wrist.configReverseSoftLimitThreshold(MercMath.degreesToEncoderTicks(WRIST_LOWER_LIMIT)*GEAR_RATIO, Constants.CTRE_TIMEOUT);
-    //wrist.configForwardSoftLimitEnable(true, Constants.CTRE_TIMEOUT);
-    //wrist.configReverseSoftLimitEnable(true, Constants.CTRE_TIMEOUT);
 
     wrist.configRemoteFeedbackFilter(pigeon.getDeviceID(), RemoteSensorSource.Pigeon_Roll, REMOTE_DEVICE_0);
     wrist.configSelectedFeedbackSensor(FeedbackDevice.RemoteSensor0, WRIST_PID_SLOT, Constants.CTRE_TIMEOUT);
@@ -103,15 +99,15 @@ public class Wrist extends SubsystemBase {
     wrist.set(ControlMode.Position, wristPos.degreePos);
   }
 
-  public double getwristPosition() {
+  public double getWristPosition() {
     return wrist.getSelectedSensorPosition();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Pigeon Roll", pigeon.getRoll());
-    SmartDashboard.putNumber("Pigeon Roll(Talon)", wrist.getSelectedSensorPosition(WRIST_PID_SLOT));
+    SmartDashboard.putNumber("Wrist Roll", pigeon.getRoll());
+    SmartDashboard.putNumber("Wrist Roll(Talon)", wrist.getSelectedSensorPosition(WRIST_PID_SLOT));
   }
 
   public enum WristPosition {
